0c817bea67781f129988a516e254b836edf08faf
Abdul Muthalib <itsabdulmuthalib@gmail.com>
2026-04-24 20:08:36 +0530

    Initial Streamlit setup completed

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Electricity AI Project", layout="wide")
+
+st.title("⚡ Electricity Consumption Forecasting and Anomaly Detection")
+st.subheader("IoT Simulated Live Data Dashboard")
+
+st.write("Project setup successful ✅")
+
+st.info("Next we will add live data, forecasting, and anomaly detection.")
